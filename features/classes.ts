41545da4c8c64b685cbc561fb6aef5dd3ade753d
Perry <perry@jamdirect.ca>
2021-01-10 12:18:07 -0500

    Classes, inheritance, super, private/public/protected.

--- features/classes.ts
+++ features/classes.ts
@@ -1,10 +1,24 @@
 class Vehicle {
+    // This is equalevant to below
+    constructor(public color: string){}
+
+    // constructor(color: string) {
+    //     this.color = color;
+    // } 
+
     protected honk(): void {
         console.log("beep beep")
     }
 }
 
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
+
+
 class ACar extends Vehicle {
+    constructor(public wheels: number, color: string){
+        super(color);
+    }
     private drive(): void {
         console.log("vroom")
     } // Can't be marked private if method in extended class is already marked public.
@@ -16,5 +30,6 @@ class ACar extends Vehicle {
     }
 }
 
-const aCar = new ACar ();
+const aCar = new ACar(4, 'red');
 aCar.startDrivingProcess();
+// aCar.honk(); This instance method won't work since its protected in Vehicle.
